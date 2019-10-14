64a4b54dd0aec8569c128927716050117afdd974
leon <you@example.com>
2019-10-14 17:39:08 +0200

    get api added

--- app/api/datasets.py
+++ app/api/datasets.py
@@ -3,9 +3,25 @@ items = {
     0: {"name": "First item"}
 }
 
-class Datasets:
-    def get(self):
+
+items = {
+    0: {"name": "First item"}
+}
+
+class DatasetsMetadataResource:
+    def read_all(self):
+        print()
+        return items
+    def create(self):
+        return items
+    def read_one(self, dataset_id):
+        print(dataset_id)
+        return items
+    def update(self, dataset_id):
+        return items
+    def delete(self, dataset_id):
+        # naredi klic na service, ki zbriše dataset iz baze
         return items
 
 
-datasets = Datasets()
+datasets = DatasetsMetadataResource()
--- app/app.py
+++ app/app.py
@@ -2,7 +2,6 @@ from connexion.resolver import RestyResolver
 import connexion
 
 
-
 if __name__ == '__main__':
     app = connexion.App(__name__, port=9090, specification_dir='swagger/')
     app.add_api('data-catalog-docs.yaml', resolver=RestyResolver('api'))
